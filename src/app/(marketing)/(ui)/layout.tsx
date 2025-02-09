34d0366f5fee61e649739f0996d2f02cb28baab7
dev-mdshahid <mdshahidulridoy@gmail.com>
2025-02-09 12:28:35 +0600

    initial setup

---
+++ src/app/(marketing)/(ui)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import '@/app/globals.css';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Linear | Your favorite issue tracker',
+  description:
+    'Linear is a state-of-the-art issue tracker app designed for efficient project management and seamless team collaboration. It streamlines workflows by offering robust features such as real-time updates, intuitive task boards, and customizable notifications. Ideal for agile teams, Linear ensures that every issue is tracked, prioritized, and resolved swiftly, enhancing productivity and maintaining project momentum. Its user-friendly interface and powerful integrations make it an essential tool for modern development teams.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
