5cb98f0d9eb1ba874e0797d0cfb268e1ec2ae5c2
dev-mdshahid <mdshahidulridoy@gmail.com>
2025-02-09 22:06:05 +0600

    personal info added on footer

--- src/app/(marketing)/(ui)/_components/Footer/Footer.tsx
+++ src/app/(marketing)/(ui)/_components/Footer/Footer.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="bg-dark-200 space-y-2 p-6 text-center">
+      <p>Made with 💝 by Md. Shahidul Islam</p>
+
+      <div className="flex items-center justify-center gap-3">
+        Find me on{" "}
+        <div className="flex items-center gap-3 text-xl">
+          <a href="https://github.com/dev-mdshahid">
+            <FaGithub />{" "}
+          </a>
+          <a href="https://www.linkedin.com/in/dev-mdshahid/">
+            <FaLinkedin />{" "}
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/app/(marketing)/(ui)/_components/Navbar/Navbar.tsx
+++ src/app/(marketing)/(ui)/_components/Navbar/Navbar.tsx
@@ -5,32 +5,34 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="max-width flex items-center justify-between p-4">
-      {" "}
-      <Image
-        src="/images/linear-logo-with-text.png"
-        alt="Linear logo"
-        height={25}
-        width={90}
-        className="h-fit"
-      />
-      {/* List of navlinks */}
-      <ul className="flex gap-7 text-sm">
-        {navData.map(({ id, name, href }) => (
-          <li key={id}>
-            <Link href={href}>{name}</Link>
-          </li>
-        ))}
-      </ul>
-      {/* Buttons */}
-      <div className="flex gap-2">
-        <Button variant="secondary" key={1}>
-          Log in
-        </Button>
-        <Button variant="primary" key={2}>
-          Sign up
-        </Button>
-      </div>
-    </nav>
+    <div className="sticky top-0 z-10 border-b border-primary-to/40 bg-black">
+      <nav className="max-width flex items-center justify-between p-4">
+        {" "}
+        <Image
+          src="/images/linear-logo-with-text.png"
+          alt="Linear logo"
+          height={25}
+          width={90}
+          className="h-fit"
+        />
+        {/* List of navlinks */}
+        <ul className="flex gap-7 text-sm">
+          {navData.map(({ id, name, href }) => (
+            <li key={id}>
+              <Link href={href}>{name}</Link>
+            </li>
+          ))}
+        </ul>
+        {/* Buttons */}
+        <div className="flex gap-2">
+          <Button variant="secondary" key={1}>
+            Log in
+          </Button>
+          <Button variant="primary" key={2}>
+            Sign up
+          </Button>
+        </div>
+      </nav>
+    </div>
   );
 }
